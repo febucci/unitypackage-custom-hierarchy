f33391494544c87171893ab9ab414ba37e1c267a
Ian G <yo.ian.g@gmail.com>
2021-05-27 12:05:06 -0400

    toggle drawing full width separators, above or below tree

--- Editor/HierarchyData.cs
+++ Editor/HierarchyData.cs
@@ -73,6 +73,8 @@ namespace Febucci.HierarchyData
             public bool enabled = true;
             public string startString = ">";
             public Color color = new Color(0, 1,1, .15f);
+            public bool fullWidth = false;
+            public bool drawUnderTree = false;
         }
         
         public SeparatorData separator;
